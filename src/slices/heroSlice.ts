87bafd63c60878ceca18045725d00a4bf03bc9f2
czcarrizales <czcarrizales@yahoo.com>
2023-08-06 20:02:59 -0700

    new state and actions (heroSlice)

--- src/slices/heroSlice.ts
+++ src/slices/heroSlice.ts
@@ -4,27 +4,19 @@ const heroSlice = createSlice({
     name: 'hero',
     initialState: {
         level: 1,
-        experience: 0,
+        experience: 90,
         health: 100,
-        maxHealth: 200,
+        maxHealth: 100,
         mana: 100,
+        money: 0,
         treasure: [],
         weapon: {
             type: 'weapon',
             name: 'fists',
-            damage: 5
+            damage: 100
         },
         equipment: [
-            {
-                type: 'weapon',
-                name: 'sword',
-                damage: 10
-            },
-            {
-                type: 'weapon',
-                name: 'hammer',
-                damage: 15
-            }
+
         ]
     },
     reducers: {
@@ -37,6 +29,9 @@ const heroSlice = createSlice({
         healToFull: (state) => {
             state.health = state.maxHealth
         },
+        gainMaxHealth: (state) => {
+            state.maxHealth += 50
+        },
         takeTreasure: (state, action) => {
             state.treasure.push(action.payload)
         },
@@ -44,16 +39,35 @@ const heroSlice = createSlice({
             state.equipment.push(action.payload)
         },
         equipWeapon: (state, action) => {
+            state.equipment.push(state.weapon)
             state.weapon = action.payload
+            state.equipment = state.equipment.filter(item => item.name !== action.payload.name)
         },
         dropWeapon: (state, action) => {
             
         },
         gainExperience: (state, action) => {
             state.experience += action.payload
+        },
+        resetExperience: (state) => {
+            state.experience = 0
+        },
+        gainLevel: (state) => {
+            state.level += 1
+        },
+        resetHero: (state) => {
+            state.level = 1
+            state.health = 100
+            state.treasure = []
+            state.weapon = {
+                type: 'weapon',
+                name: 'fists',
+                damage: 5
+            }
+            state.equipment = []
         }
     }
 })
 
-export const {heroTakeDamage, lowerMana, healToFull, takeTreasure, takeWeapon, equipWeapon, gainExperience} = heroSlice.actions;
+export const {heroTakeDamage, lowerMana, healToFull, takeTreasure, takeWeapon, equipWeapon, gainExperience, gainLevel, resetExperience, gainMaxHealth} = heroSlice.actions;
 export default heroSlice.reducer
