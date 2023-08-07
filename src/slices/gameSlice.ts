8346ed18509b93cea00fd09dd4e952404bd04805
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-07 02:12:34 -0700

    adding currentWorld state (gameSlice)

--- src/slices/gameSlice.ts
+++ src/slices/gameSlice.ts
@@ -3,14 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const gameSlice = createSlice({
     name: 'game',
     initialState: {
-        resettingGame: false
+        resettingGame: false,
+        currentWorld: 1
     },
     reducers: {
         resetGame: (state) => {
             state.resettingGame = !state.resettingGame
+        },
+        setCurrentWorld: (state) => {
+            state.currentWorld += 1
         }
     }
 })
 
-export const {resetGame} = gameSlice.actions;
+export const {resetGame, setCurrentWorld} = gameSlice.actions;
 export default gameSlice.reducer
