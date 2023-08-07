e191ff2dea46aacb770a11ef1066313a1a245ef5
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-07 02:12:22 -0700

    more enemies and bosses based on world

--- src/slices/enemySlice.ts
+++ src/slices/enemySlice.ts
@@ -5,46 +5,106 @@ const enemyTypes = [
         name: 'rat',
         health: 20,
         attack: 5,
-        experience: 10
+        experience: 10,
+        world: 1
     },
     {
         name: 'wolf',
         health: 40,
         attack: 10,
-        experience: 20
+        experience: 20,
+        world: 1
     },
     {
-        name: 'ogre',
+        name: 'card knight',
         health: 100,
         attack: 20,
-        experience: 50
+        experience: 50,
+        world: 2
     },
+    {
+        name: 'little witch',
+        health: 100,
+        attack: 20,
+        experience: 50,
+        world: 2
+    },
+    {
+        name: 'frost spider',
+        health: 150,
+        attack: 30,
+        experience: 60,
+        world: 3
+    },
+    {
+        name: 'ice picker',
+        health: 150,
+        attack: 30,
+        experience: 60,
+        world: 3
+    }
 ]
 
 const bosses = [
     {
-        name: 'black knight',
-        health: 200,
+        name: 'plant king',
+        health: 100,
         attack: 20,
-        experience: 100
+        experience: 100,
+        world: 1
+    },
+    {
+        name: 'misguided knight',
+        health: 100,
+        attack: 20,
+        experience: 100,
+        world: 1
+    },
+    {
+        name: 'jester',
+        health: 200,
+        attack: 40,
+        experience: 100,
+        world: 2
     },
     {
         name: 'hazel witch',
-        health: 400,
+        health: 200,
         attack: 40,
-        experience: 200
+        experience: 200,
+        world: 2
+    },
+    {
+        name: 'great yeti',
+        health: 500,
+        attack: 60,
+        experience: 300,
+        world: 3
+    },
+    {
+        name: 'frozen overlord',
+        health: 500,
+        attack: 60,
+        experience: 300,
+        world: 3
     }
 ]
 
-const getRandomEnemy = () => {
-    const randomIndex = Math.floor(Math.random() * enemyTypes.length)
-    const randomEnemy = enemyTypes[randomIndex]
+const getRandomEnemy = (world) => {
+    const filteredEnemies = enemyTypes.filter((enemy) => {
+        return enemy.world == world
+    })
+    const randomIndex = Math.floor(Math.random() * filteredEnemies.length)
+    const randomEnemy = filteredEnemies[randomIndex]
     return randomEnemy
 }
 
-const getRandomBoss = () => {
-    const randomIndex = Math.floor(Math.random() * bosses.length)
-    const randomBoss = bosses[randomIndex]
+const getRandomBoss = (world) => {
+    const filteredBosses = bosses.filter((boss) => {
+        return boss.world == world
+    })
+    const randomIndex = Math.floor(Math.random() * filteredBosses.length)
+    const randomBoss = filteredBosses[randomIndex]
     return randomBoss
 }
 
@@ -69,11 +129,11 @@ const enemySlice = createSlice({
         enemyReset: (state) => {
             state.health = 100
         },
-        setEnemyType: (state) => {
-            state.currentEnemy = getRandomEnemy()
+        setEnemyType: (state, action) => {
+            state.currentEnemy = getRandomEnemy(action.payload)
         },
-        setBossType: (state) => {
-            state.currentEnemy = getRandomBoss()
+        setBossType: (state, action) => {
+            state.currentEnemy = getRandomBoss(action.payload)
         }
     }
 })
