3040cdf79d36e64aeb1622907b5df88b63ed2410
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-07 01:35:27 -0700

    if gamestate resets, then set visited to false

--- src/RoomButton.tsx
+++ src/RoomButton.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { goToRandomRoom, setInRoom } from './slices/roomSlice'
 import './RoomButton.css'
 
 const RoomButton = ({inRoom}) => {
+    const gameState = useSelector(state => state.game)
     const dispatch = useDispatch()
     const [visited, setVisited] = useState(false)
     const handleRandomRoom = () => {
@@ -15,6 +16,13 @@ const RoomButton = ({inRoom}) => {
     useEffect(() => {
         console.log(visited)
     }, [visited])
+
+    useEffect(() => {
+      if (gameState.resettingGame == true) {
+        console.log('resetting game is true!')
+        setVisited(false)
+      }
+    }, [gameState])
   return (
     <div>
         <button onClick={() => {handleRandomRoom(), setVisited(true)}} disabled={visited || inRoom} className='room-button'>?</button>
