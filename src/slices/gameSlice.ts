39a55435f51fa382aa74d350054a13a506539872
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-08 19:51:53 -0700

    add functions to resetCurrentWorld and setGameOver

--- src/slices/gameSlice.ts
+++ src/slices/gameSlice.ts
@@ -4,17 +4,24 @@ const gameSlice = createSlice({
     name: 'game',
     initialState: {
         resettingGame: false,
-        currentWorld: 1
+        currentWorld: 1,
+        gameOver: false
     },
     reducers: {
-        resetGame: (state) => {
-            state.resettingGame = !state.resettingGame
+        resetGame: (state, action) => {
+            state.resettingGame = action.payload
         },
         setCurrentWorld: (state) => {
             state.currentWorld += 1
+        },
+        resetCurrentWorld: (state) => {
+            state.currentWorld = 1
+        },
+        setGameOver: (state, action) => {
+            state.gameOver = action.payload
         }
     }
 })
 
-export const {resetGame, setCurrentWorld} = gameSlice.actions;
+export const {resetGame, setCurrentWorld, resetCurrentWorld, setGameOver} = gameSlice.actions;
 export default gameSlice.reducer
