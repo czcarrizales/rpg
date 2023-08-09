b5ae2cfe5b940d62416fda4f7cad7f950997667a
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-08 19:56:06 -0700

    show more stats and states on Hero display

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -5,7 +5,7 @@ import { lowerMana } from './slices/heroSlice'
 import './Hero.css'
 import { setPlayerTurn } from './slices/battleSlice'
 
-const Hero = ({showEquipment, setShowEquipment, showBackpack, setShowBackpack, inBattle}) => {
+const Hero = ({ showEquipment, setShowEquipment, showBackpack, setShowBackpack }) => {
   const heroLevel = useSelector(state => state.hero.level)
   const heroExperience = useSelector(state => state.hero.experience)
   const heroMaxHealth = useSelector(state => state.hero.maxHealth)
@@ -14,15 +14,14 @@ const Hero = ({showEquipment, setShowEquipment, showBackpack, setShowBackpack, i
   const heroMoney = useSelector(state => state.hero.money)
   const heroTreasure = useSelector(state => state.hero.treasure)
   const heroWeapon = useSelector(state => state.hero.weapon)
+  const heroArmor = useSelector(state => state.hero.armor)
   const battleTurn = useSelector(state => state.battle.playerTurn)
+  const inBattle = useSelector(state => state.battle.inBattle)
+  const currentWorld = useSelector(state => state.game.currentWorld)
   const dispatch = useDispatch()
 
   const handleAttackEnemy = () => {
     dispatch(enemyTakeDamage(heroWeapon.damage))
-    document.documentElement.style.setProperty('--body-bg-color', 'var(--attack-bg-color)');
-    setTimeout(() => {
-      document.documentElement.style.setProperty('--body-bg-color', '--body-bg-color');
-    }, 500);
     dispatch(setPlayerTurn(false))
   }
 
@@ -33,22 +32,26 @@ const Hero = ({showEquipment, setShowEquipment, showBackpack, setShowBackpack, i
     } else {
 
     }
-    
+
   }
   return (
     <div id='hero-container'>
       <h1>Hero</h1>
+      <p>Current World: {currentWorld}</p>
       <p>Level: {heroLevel}</p>
       <p>XP: {heroExperience}</p>
       <p>Health: {heroHealth}/{heroMaxHealth}</p>
       <p>Mana: {heroMana}</p>
       <p>Weapon: {heroWeapon.name} (dmg: {heroWeapon.damage})</p>
+      <p>Armor: {heroArmor.name ? heroArmor.name : 'None'} (defense: {heroArmor.defense ? heroArmor.defense : 0})</p>
       <p>Money: {heroMoney}</p>
-
-<button onClick={handleAttackEnemy} className='hero-button' disabled={!battleTurn}>Attack</button>
+      <div id="hero-buttons">
+      <button onClick={handleAttackEnemy} className='hero-button' disabled={!battleTurn || !inBattle}>Attack</button>
       {heroHealth == 0 && <p>Hero is dead!</p>}
       <button className='hero-button' onClick={() => setShowEquipment(!showEquipment)} disabled={!battleTurn}>Equipment</button>
       <button className='hero-button' onClick={() => setShowBackpack(!showBackpack)} disabled={!battleTurn}>Backpack</button>
+      </div>
+      
     </div>
   )
 }
