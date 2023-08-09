dce4a5d018abd95773e3b6cd64f273ab686f2ea7
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-08 19:56:15 -0700

    creating ShopRoom

--- src/Shop.tsx
+++ src/Shop.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const Shop = () => {
+  const shopItems = [
+    {
+      type: 'potion',
+      name: 'healing potion',
+      gainHealth: 20
+    },
+    {
+      type: 'potion',
+      name: 'mana potion',
+      gainMana: 20
+    }
+  ]
   return (
     <div>
-        
+        <h1>Shop</h1>
+        {}
     </div>
   )
 }
