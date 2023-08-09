1a4cc8360a2b95abb4a7750e2739f3afb5e7240a
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-08 19:55:37 -0700

    creating filter for equipment

--- src/Equipment.tsx
+++ src/Equipment.tsx
@@ -1,26 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { equipWeapon } from './slices/heroSlice'
+import { equipArmor, equipWeapon } from './slices/heroSlice'
 
 const Equipment = () => {
     const heroEquipment = useSelector(state => state.hero.equipment)
+    const [equipmentFilter, setEquipmentFilter] = useState('')
     console.log(heroEquipment)
     const dispatch = useDispatch()
 
     const handleWeaponEquip = (weapon) => {
         dispatch(equipWeapon(weapon))
     }
+    const handleArmorEquip = (armor) => {
+        dispatch(equipArmor(armor))
+    }
+    const showAllEquipment = () => {
+        return (
+            heroEquipment.map((item) => {
+                return (
+                    <div>
+                        <h2>{item.name} ({item.type})</h2>
+                        {item.damage && <p>Damage: {item.damage}</p>}
+                        {item.defense && <p>Defense: {item.defense}</p>}
+                        {item.type == 'weapon' && <button onClick={() => handleWeaponEquip(item)}>Equip</button>}
+                        {item.type == 'armor' && <button onClick={() => handleArmorEquip(item)}>Equip</button>}
+                        
+                    </div>
+    
+            )
+            })
+        )
+    }
+
+    const showWeapons = () => {
+        const allWeapons = heroEquipment.filter(item => item.type !== 'weapon')
+        return (
+            allWeapons.map((item) => {
+                return (
+                    <div><h2>{item.name}</h2>
+                    <p>{item.damage}</p></div>
+                )
+            })
+        )
+
+    }
     return (
         <div>
             <h1>Equipment</h1>
-            {heroEquipment.map((item) => (
-                <div>
-                    <h2>{item.name}</h2>
-                    <p>{item.damage}</p>
-                    <button onClick={() => handleWeaponEquip(item)}>Equip</button>
-                </div>
-
-            ))}
+            {showAllEquipment()}
         </div>
     )
 }
