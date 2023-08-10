7327c5d7b6a166310c5cd22cc5d51c51f2cf85e6
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-09 19:49:21 -0700

    go back from Backpack to Hero

--- src/Backpack.tsx
+++ src/Backpack.tsx
@@ -1,12 +1,18 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './Backpack.css'
 import { RootState } from './store'
+import { setShowBackpack } from './slices/gameSlice'
 
 const Backpack = () => {
     const heroTreasure = useSelector((state: RootState) => state.hero.treasure)
+    const dispatch = useDispatch()
     return (
         <div id='backpack-container'>
+            <div>
             <h1>Backpack</h1>
+            <button onClick={() => dispatch(setShowBackpack(false))}>Go Back</button>
+            </div>
+            
             {
             heroTreasure.length > 0
             ?
