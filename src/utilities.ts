adab1cf6fe65e4867425ee25b696bfa2d2aab814
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-09 19:47:17 -0700

    create file for utility functions

---
+++ src/utilities.ts
@@ -0,0 +1,44 @@
+import { setEnemyIsAttacked } from "./slices/enemySlice";
+import { setHeroIsAttacked } from "./slices/heroSlice";
+
+export const enemyTakeDamageFlash = (dispatch) => {
+    setTimeout(() => {
+        dispatch(setEnemyIsAttacked(true));
+      
+        setTimeout(() => {
+          dispatch(setEnemyIsAttacked(false));
+      
+          setTimeout(() => {
+            dispatch(setEnemyIsAttacked(true));
+      
+            setTimeout(() => {
+              dispatch(setEnemyIsAttacked(false));
+            }, 80); // Second flash duration is 100 milliseconds
+      
+          }, 80); // Wait for 100 milliseconds before the second flash
+      
+        }, 80); // First flash duration is 100 milliseconds
+      
+      }, 80); // Wait for 100 milliseconds before the first flash
+}
+
+export const heroTakeDamageFlash = (dispatch) => {
+    setTimeout(() => {
+        dispatch(setHeroIsAttacked(true));
+      
+        setTimeout(() => {
+          dispatch(setHeroIsAttacked(false));
+      
+          setTimeout(() => {
+            dispatch(setHeroIsAttacked(true));
+      
+            setTimeout(() => {
+              dispatch(setHeroIsAttacked(false));
+            }, 80); // Second flash duration is 100 milliseconds
+      
+          }, 80); // Wait for 100 milliseconds before the second flash
+      
+        }, 80); // First flash duration is 100 milliseconds
+      
+      }, 80); // Wait for 100 milliseconds before the first flash
+}
