c97ad979ec3edd3505c58a9931469558517a7d05
czcarrizales <czcarrizales@yahoo.com>
2023-08-11 12:47:07 -0700

    AfterBattle component and css create

---
+++ src/AfterBattle.tsx
@@ -0,0 +1,16 @@
+import { useDispatch } from "react-redux"
+import { setAfterBattle } from "./slices/gameSlice"
+import './AfterBattle.css'
+
+const AfterBattle = (props: any) => {
+    const dispatch = useDispatch()
+  return (
+    <div className="after-battle-container">
+        <p>{props.name} defeated!</p>
+        <p>XP Gained: {props.xp}</p>
+        <button onClick={() => dispatch(setAfterBattle(false))}>Continue</button>
+    </div>
+  )
+}
+
+export default AfterBattle
