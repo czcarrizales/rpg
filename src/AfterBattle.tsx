709a96eb871af9899c5ae5d441ac026b187b2dd5
czcarrizales <czcarrizales@yahoo.com>
2023-08-11 17:42:21 -0700

    showing money gained after battle

--- src/AfterBattle.tsx
+++ src/AfterBattle.tsx
@@ -8,6 +8,7 @@ const AfterBattle = (props: any) => {
     <div className="after-battle-container">
         <p>{props.name} defeated!</p>
         <p>XP Gained: {props.xp}</p>
+        <p>Money Gained: {props.money}</p>
         <button onClick={() => dispatch(setAfterBattle(false))}>Continue</button>
     </div>
   )
