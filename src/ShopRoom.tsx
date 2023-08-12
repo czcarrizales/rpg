52eb3a2bf2535f1acf4dcfe28c0f8d019808fa5a
czcarrizales <czcarrizales@yahoo.com>
2023-08-12 14:40:32 -0700

    add buyItem to ShopRoom

--- src/ShopRoom.tsx
+++ src/ShopRoom.tsx
@@ -1,29 +1,33 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './ShopRoom.css'
 import { goToMapRoom, setInRoom } from './slices/roomSlice'
+import { RootState } from './store'
+import { buyItemForBackpack } from './slices/heroSlice'
 const ShopRoom = () => {
   const dispatch = useDispatch()
+  const shopItems = useSelector((state: RootState) => state.shop.shopItems)
+  const heroMoney = useSelector((state: RootState) => state.hero.money)
   const goBack = () => {
     dispatch(goToMapRoom())
     dispatch(setInRoom(false))
   }
-  // const shopItems = [
-  //   {
-  //     type: 'potion',
-  //     name: 'healing potion',
-  //     gainHealth: 20
-  //   },
-  //   {
-  //     type: 'potion',
-  //     name: 'mana potion',
-  //     gainMana: 20
-  //   }
-  // ]
+  const buyItem = (money: any, item: any) => {
+    if (heroMoney - money >= 0) {
+      dispatch(buyItemForBackpack({money: money, item: item}))
+    }
+    
+  }
   return (
     <div className='shop-room-container'>
         <h1>Shop</h1>
         <button onClick={() => goBack()}>go back</button>
-        {}
+        {shopItems.map(item => (
+          <div>
+            <p>{item.name}</p>
+            <p>{item.money}</p>
+            <button onClick={() => buyItem(item.money, item)}>Buy</button>
+            </div>
+        ))}
     </div>
   )
 }
