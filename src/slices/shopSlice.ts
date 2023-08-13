7c58d001467174c25c389d0765eb51d600641007
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-13 10:11:44 -0700

    give items type and actions

--- src/slices/shopSlice.ts
+++ src/slices/shopSlice.ts
@@ -3,23 +3,32 @@ import { createSlice } from "@reduxjs/toolkit";
 const allShopItems = [
     {
         name: 'health potion',
-        money: 20
+        money: 20,
+        points: 20,
+        type: 'USABLE',
+        action: 'RAISEHEALTH'
     },
     {
         name: 'mana potion',
-        money: 20
+        money: 20,
+        points: 20,
+        type: 'USABLE',
+        action: 'RAISEMANA'
     }
 ]
 
 const shopSlice = createSlice({
     name: 'shop',
     initialState: {
-        shopItems: allShopItems
+        shopItems: allShopItems,
+        inShop: false
     },
     reducers: {
-        
+        setInShop: (state, action) => {
+            state.inShop = action.payload
+        }
     }
 })
 
-export const {} = shopSlice.actions;
+export const {setInShop} = shopSlice.actions;
 export default shopSlice.reducer;
