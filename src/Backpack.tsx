6868a0dd84a8cc9c0a9a5f71c90f0d27552330d4
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-13 10:15:26 -0700

    handleItemUse for different items in Backpack

--- src/Backpack.tsx
+++ src/Backpack.tsx
@@ -2,19 +2,40 @@ import { useDispatch, useSelector } from 'react-redux'
 import './Backpack.css'
 import { RootState } from './store'
 import { setShowBackpack } from './slices/gameSlice'
+import { gainMoney, raiseHeroHealth, raiseMana, sellItemFromBackpack } from './slices/heroSlice'
 
 const Backpack = () => {
     const heroBackpack = useSelector((state: RootState) => state.hero.backpack)
-    const heroMoney = useSelector((state: RootState) => state.hero.money)
+    const inShop = useSelector((state: RootState) => state.shop.inShop)
     const dispatch = useDispatch()
+    const handleItemUse = (item: any) => {
+        if (item.type == 'USABLE') {
+            switch (item.action) {
+                case 'RAISEHEALTH':
+                    dispatch(raiseHeroHealth(item.points))
+                    break;
+                case 'RAISEMANA':
+                    dispatch(raiseMana(item.points))
+                    break;
+                default:
+                    break;
+            }
+        } else if (item.type == 'TREASURE') {
+            dispatch(sellItemFromBackpack(item))
+            dispatch(gainMoney(item.money))
+        }
+    }
+
+    const handleSellItem = (item: any, money: any) => {
+        dispatch(sellItemFromBackpack(item))
+        dispatch(gainMoney(money))
+    }
     return (
         <div id='backpack-container'>
             <div>
             <h1>Backpack</h1>
             <button className='hero-button' onClick={() => dispatch(setShowBackpack(false))}>Go Back</button>
-            Money: {heroMoney}
             </div>
-            
             {
             heroBackpack.length > 0
             ?
@@ -22,6 +43,8 @@ const Backpack = () => {
                 <div>
                     <h2>{item.name}</h2>
                     <p>Value: {item.money}</p>
+                    {item.type && <button onClick={() => handleItemUse(item)}>Use</button>}
+                    {inShop && <button onClick={() => handleSellItem(item, item.money)}>Sell</button>}
                 </div>
 
             ))
