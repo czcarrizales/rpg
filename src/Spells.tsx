c60e36b95b8ec7327595a109e6b29753b3cd90a9
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-14 12:20:27 -0700

    new Spell display for Hero

--- src/Spells.tsx
+++ src/Spells.tsx
@@ -1,4 +1,4 @@
-import { setShowSpells } from './slices/gameSlice'
+import { setInAnimation, setShowSpells } from './slices/gameSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import './Spells.css'
 import { lowerMana, raiseHeroHealth } from './slices/heroSlice'
@@ -15,33 +15,63 @@ const Spells = () => {
     const inBattle = useSelector((state: RootState) => state.battle.inBattle)
     const inAnimation = useSelector((state: RootState) => state.game.inAnimation)
     const battleTurn = useSelector((state: RootState) => state.battle.playerTurn)
-    const healHero = () => {
-        if (heroMana >= 5 && heroHealth + 5 <= heroMaxHealth) {
-            dispatch(raiseHeroHealth(20))
-            dispatch(lowerMana(5))
-            dispatch(setPlayerTurn(false))
+    interface Spell {
+        type: string | null;
+        name: string | null;
+        points: number | null;
+        mana: number | null;
+      }
+      const heroSpells = useSelector((state: RootState) => state.hero.spells)
+      const handleAttackEnemy = async (spellDamage: number)  => {
+        dispatch(setInAnimation(true))
+        dispatch(enemyTakeDamage(spellDamage))
+        enemyTakeDamageFlash(dispatch)
+        await new Promise((resolve) => setTimeout(resolve, 500))
+        dispatch(setPlayerTurn(false))
+        dispatch(setInAnimation(false))
+      }
+      const handleSpellUse = (spell: Spell) => {
+        switch (spell.type) {
+            case 'HEAL':
+                dispatch(raiseHeroHealth(spell.points))
+                dispatch(lowerMana(spell.mana))
+                break
+            case 'DAMAGE':
+                handleAttackEnemy(spell.points!)
+                dispatch(lowerMana(spell.mana))
+                break
+            default:
+                break; 
         }
-    }
-    const fireball = () => {
-        if (heroMana >= 10) {
-            dispatch(enemyTakeDamage(20))
-            enemyTakeDamageFlash(dispatch)
-            dispatch(lowerMana(10))
-            dispatch(setPlayerTurn(false))
-        }
-    }
-    const lightningBolt = () => {
-        if (heroMana >= 5) {
-            dispatch(enemyTakeDamage(10))
-            enemyTakeDamageFlash(dispatch)
-            dispatch(lowerMana(5))
-            dispatch(setPlayerTurn(false))
-        }
-    }
+      }
+    // const healHero = () => {
+    //     if (heroMana >= 5 && heroHealth + 5 <= heroMaxHealth) {
+    //         dispatch(raiseHeroHealth(20))
+    //         dispatch(lowerMana(5))
+    //         dispatch(setPlayerTurn(false))
+    //     }
+    // }
+    // const fireball = () => {
+    //     if (heroMana >= 10) {
+    //         dispatch(enemyTakeDamage(20))
+    //         enemyTakeDamageFlash(dispatch)
+    //         dispatch(lowerMana(10))
+    //         dispatch(setPlayerTurn(false))
+    //     }
+    // }
+    // const lightningBolt = () => {
+    //     if (heroMana >= 5) {
+    //         dispatch(enemyTakeDamage(10))
+    //         enemyTakeDamageFlash(dispatch)
+    //         dispatch(lowerMana(5))
+    //         dispatch(setPlayerTurn(false))
+    //     }
+    // }
     return (
         <div id='spell-buttons'>
             <button className='hero-button' onClick={() => dispatch(setShowSpells(false))}>Go Back</button>
-            <button className='hero-button' onClick={() => healHero()} disabled={!battleTurn || !inBattle || inAnimation}>
+            {heroSpells.map(spell => (<button onClick={() => handleSpellUse(spell)} disabled={!battleTurn || !inBattle || inAnimation}>{spell.name}</button>))}
+            {/* <button className='hero-button' onClick={() => healHero()} disabled={!battleTurn || !inBattle || inAnimation}>
                 <div>Cure</div>
                 <div>(Mana: 5 / Heal: 20)</div>
                 </button>
@@ -52,7 +82,7 @@ const Spells = () => {
             <button className='hero-button' onClick={() => lightningBolt()} disabled={!battleTurn || !inBattle || inAnimation}>
                 <div>Lightning Bolt</div>
                 <div>(Mana: 5 / Damage: 15)</div>
-                </button>
+                </button> */}
         </div>
 
     )
