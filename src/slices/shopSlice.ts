70b0966e717675b34692e5fc19673d550a8caf7c
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-14 12:14:59 -0700

    images for shopItems and other reducers

--- src/slices/shopSlice.ts
+++ src/slices/shopSlice.ts
@@ -1,19 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {v4 as uuidv4} from 'uuid';
 
 const allShopItems = [
     {
+        id: uuidv4(),
         name: 'health potion',
         money: 20,
         points: 20,
         type: 'USABLE',
-        action: 'RAISEHEALTH'
+        action: 'RAISEHEALTH',
+        image: '/images/items/potions/health-potion.png'
     },
     {
+        id: uuidv4(),
         name: 'mana potion',
         money: 20,
         points: 20,
         type: 'USABLE',
-        action: 'RAISEMANA'
+        action: 'RAISEMANA',
+        image: '/images/items/potions/mana-potion.png'
     }
 ]
 
@@ -26,9 +31,18 @@ const shopSlice = createSlice({
     reducers: {
         setInShop: (state, action) => {
             state.inShop = action.payload
+        },
+        removeFromShop: (state, action) => {
+            state.shopItems = state.shopItems.filter(item => item.id !== action.payload)
+        },
+        restockShop: (state) => {
+            state.shopItems = allShopItems;
+        },
+        resetIdOnAllShopItems: (state) => {
+            state.shopItems = state.shopItems.map(item => ({...item, id: uuidv4()}))
         }
     }
 })
 
-export const {setInShop} = shopSlice.actions;
+export const {setInShop, removeFromShop, resetIdOnAllShopItems, restockShop} = shopSlice.actions;
 export default shopSlice.reducer;
