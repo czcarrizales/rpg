aa04033c4e6617e4eeb02e5a902d5769de6ef138
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-14 12:18:56 -0700

    new handlers for item use from backpack

--- src/Backpack.tsx
+++ src/Backpack.tsx
@@ -2,20 +2,36 @@ import { useDispatch, useSelector } from 'react-redux'
 import './Backpack.css'
 import { RootState } from './store'
 import { setShowBackpack } from './slices/gameSlice'
-import { gainMoney, raiseHeroHealth, raiseMana, sellItemFromBackpack } from './slices/heroSlice'
+import { gainMoney, healToFull, raiseHeroHealth, raiseMana, sellItemFromBackpack, setMana } from './slices/heroSlice'
 
 const Backpack = () => {
     const heroBackpack = useSelector((state: RootState) => state.hero.backpack)
     const inShop = useSelector((state: RootState) => state.shop.inShop)
+    const heroStats = useSelector((state: RootState) => state.hero)
     const dispatch = useDispatch()
     const handleItemUse = (item: any) => {
+        const maxHealth = heroStats.maxHealth;
+        const maxMana = heroStats.maxMana;
+        const currentHealth = heroStats.health;
+        const currentMana = heroStats.mana;
         if (item.type == 'USABLE') {
             switch (item.action) {
                 case 'RAISEHEALTH':
-                    dispatch(raiseHeroHealth(item.points))
+                    const newHealth = currentHealth + item.points;
+                    if (newHealth > maxHealth) {
+                        dispatch(healToFull())
+                    } else {
+                        dispatch(raiseHeroHealth(item.points))
+                    }
                     break;
                 case 'RAISEMANA':
-                    dispatch(raiseMana(item.points))
+                    const newMana = currentMana + item.points;
+                    if (newMana > maxMana) {
+                        dispatch(setMana(maxMana))
+                    } else {
+                        dispatch(raiseMana(item.points))
+                    }
+                    
                     break;
                 default:
                     break;
@@ -40,11 +56,11 @@ const Backpack = () => {
             heroBackpack.length > 0
             ?
             heroBackpack.map((item) => (
-                <div>
-                    <h2>{item.name}</h2>
-                    <p>Value: {item.money}</p>
-                    {item.type && <button onClick={() => handleItemUse(item)}>Use</button>}
+                <div className='backpack-item-details'>
+                    <img className='backpack-item-image' src={item.image} alt="" />
+                    <p>{item.name} (Value: {item.money})</p>
                     {inShop && <button onClick={() => handleSellItem(item, item.money)}>Sell</button>}
+                    {item.type && <button onClick={() => handleItemUse(item)}>Use</button>}
                 </div>
 
             ))
