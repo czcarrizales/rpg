f555188fcb28db98b3b163a146134e9108e762c8
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-14 21:29:33 -0700

    show more stats

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -8,14 +8,16 @@ const Stats = () => {
     const heroStats = useSelector((state: RootState) => state.hero)
   return (
     <div>
-        <>
-                  <h2>Stats</h2>
+        <div className="stats-container-top">
+            <h2>Stats</h2>
+            <button className='stats-container-back-button hero-button' onClick={() => dispatch(setShowStats(false))}>Back</button>
+        </div>
+                  <p>Level: {heroStats.level}</p>
                   <p>Health: {heroStats.health}/{heroStats.maxHealth}</p>
                   <p>Mana: {heroStats.mana}/{heroStats.maxMana}</p>
                   <p>Attack: {heroStats.attack} {heroStats.weapon.damage && `(+${heroStats.weapon.damage})` }</p>
                   <p>Defense: {heroStats.defense} {heroStats.armor.defense && `(+${heroStats.armor.defense})`}</p>
-                  <button onClick={() => dispatch(setShowStats(false))}>Go back</button>
-                  </>
+                  
     </div>
   )
 }
