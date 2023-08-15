ca75c252ea15a115ee4970787451f097cbc8dffe
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-14 21:30:17 -0700

    css for Spell and remove unneeded code

--- src/Spells.tsx
+++ src/Spells.tsx
@@ -41,45 +41,14 @@ const Spells = () => {
                 break; 
         }
       }
-    // const healHero = () => {
-    //     if (heroMana >= 5 && heroHealth + 5 <= heroMaxHealth) {
-    //         dispatch(raiseHeroHealth(20))
-    //         dispatch(lowerMana(5))
-    //         dispatch(setPlayerTurn(false))
-    //     }
-    // }
-    // const fireball = () => {
-    //     if (heroMana >= 10) {
-    //         dispatch(enemyTakeDamage(20))
-    //         enemyTakeDamageFlash(dispatch)
-    //         dispatch(lowerMana(10))
-    //         dispatch(setPlayerTurn(false))
-    //     }
-    // }
-    // const lightningBolt = () => {
-    //     if (heroMana >= 5) {
-    //         dispatch(enemyTakeDamage(10))
-    //         enemyTakeDamageFlash(dispatch)
-    //         dispatch(lowerMana(5))
-    //         dispatch(setPlayerTurn(false))
-    //     }
-    // }
+  
     return (
         <div id='spell-buttons'>
-            <button className='hero-button' onClick={() => dispatch(setShowSpells(false))}>Go Back</button>
-            {heroSpells.map(spell => (<button onClick={() => handleSpellUse(spell)} disabled={!battleTurn || !inBattle || inAnimation}>{spell.name}</button>))}
-            {/* <button className='hero-button' onClick={() => healHero()} disabled={!battleTurn || !inBattle || inAnimation}>
-                <div>Cure</div>
-                <div>(Mana: 5 / Heal: 20)</div>
-                </button>
-            <button className='hero-button' onClick={() => fireball()} disabled={!battleTurn || !inBattle || inAnimation}>
-                <div>Fireball</div>
-                <div>(Mana: 10 / Damage: 25)</div>
-                </button>
-            <button className='hero-button' onClick={() => lightningBolt()} disabled={!battleTurn || !inBattle || inAnimation}>
-                <div>Lightning Bolt</div>
-                <div>(Mana: 5 / Damage: 15)</div>
-                </button> */}
+            <button className='hero-button' onClick={() => dispatch(setShowSpells(false))}>Back</button>
+            {heroSpells.map(spell => (<button className='hero-button' onClick={() => handleSpellUse(spell)} disabled={!battleTurn || !inBattle || inAnimation}>{spell.name}</button>))}
+            <button className='hero-button'>fireball</button>
+            <button className='hero-button'>light heal</button>
+            <button className='hero-button'>d</button>
         </div>
 
     )
