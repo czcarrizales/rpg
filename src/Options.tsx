175ebf4bc8f0448429f7101292a439772b04e66c
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-15 00:42:21 -0700

    creating Options

--- src/Options.tsx
+++ src/Options.tsx
@@ -1,6 +1,20 @@
+import { useDispatch, useSelector } from "react-redux"
+import { setShowOptions } from "./slices/gameSlice"
+import './Options.css'
+import { RootState } from "./store"
+import { setMusicVolume } from "./slices/soundsSlice"
+
 const Options = () => {
+    const dispatch = useDispatch()
+    const musicVolume = useSelector((state: RootState) => state.sounds.musicVolume)
   return (
-    <div>Options</div>
+    <div>
+        <div className="options-container-top">
+            <h2>Options</h2>
+            <button className='options-container-back-button hero-button' onClick={() => dispatch(setShowOptions(false))}>Back</button>
+        </div>
+                  <button onClick={() => dispatch(setMusicVolume(musicVolume + 0.1))}>Raise Music Volume</button>
+    </div>
   )
 }
 
