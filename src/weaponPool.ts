56e47ccd787f3bfa315bbcefcc94db48af387a55
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-18 09:39:52 -0700

    create new weaponFile

---
+++ src/weaponPool.ts
@@ -0,0 +1,38 @@
+export const allWeaponsPool = [
+    {
+        type: 'weapon',
+        name: 'wooden sword',
+        damage: 2,
+        image: '/images/weapons/wooden-sword.png'
+    },
+    {
+        type: 'weapon',
+        name: 'bronze sword',
+        damage: 4,
+        image: '/images/weapons/bronze-sword.png'
+    },
+    {
+        type: 'weapon',
+        name: 'iron sword',
+        damage: 6,
+        image: '/images/weapons/iron-sword.png'
+    },
+    {
+      type: 'weapon',
+        name: 'marble sword',
+        damage: 8,
+        image: '/images/weapons/marble-sword.png'
+    },
+    {
+      type: 'weapon',
+        name: 'obsidian sword',
+        damage: 10,
+        image: '/images/weapons/obsidian-sword.png'
+    },
+    {
+      type: 'weapon',
+        name: 'gold sword',
+        damage: 12,
+        image: '/images/weapons/gold-sword.png'
+    }
+]
