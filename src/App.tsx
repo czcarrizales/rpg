f466271b3fdea43b5bf21ca3cdee5d39b7a69180
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-18 09:51:57 -0700

    importing more music to play

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import Hero from './Hero'
 import TreasureRoom from './TreasureRoom'
 import HealingRoom from './HealingRoom'
 import BossRoom from './BossRoom'
-import { useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { goToBossRoom, goToHealingRoom, goToShopRoom, setBossBattle, setInRoom, setRandomRooms } from './slices/roomSlice'
 import WeaponRoom from './WeaponRoom'
 import SpellRoom from './SpellRoom'
 import RoomButton from './RoomButton'
 import { gainLevel } from './slices/heroSlice'
-import {  resetGame, setGameOver, setLevelingUp } from './slices/gameSlice'
+import { resetGame, setGameOver, setLevelingUp } from './slices/gameSlice'
 import { GameOver } from './GameOver'
 import ArmorRoom from './ArmorRoom'
 import { RootState } from './store'
@@ -23,6 +23,12 @@ import lordOfTheCastle from '../public/music/boss/lord-of-the-castle.mp3'
 import theCorrupted from '../public/music/boss/the-corrupted.mp3'
 import edgeOfExistence from '../public/music/world/edge-of-existence.mp3'
 import danceToTheDeath from '../public/music/world/dance-to-the-death.mp3'
+import beach from '../public/music/world/beach.mp3'
+import squirmingEvil from '../public/music/boss/squirmingEvil.mp3'
+import monstrousMonstro from '../public/music/world/monstrousMonstro.mp3'
+import tensionRising from '../public/music/boss/tensionRising.mp3'
+import scherzo from '../public/music/world/scherzo.mp3'
+import the13thReflection from '../public/music/boss/the13thReflection.mp3'
 import ShopRoom from './ShopRoom'
 
 function App() {
@@ -75,10 +81,16 @@ function App() {
     if (currentRoom === 'bossRoom' && currentWorld === 1) {
       playMusic(boss1)
     } else if (currentRoom === 'bossRoom' && currentWorld === 2) {
-      playMusic(boss2)
+      playMusic(squirmingEvil)
+    } else if (currentRoom === 'bossRoom' && currentWorld === 3) {
+      playMusic(tensionRising)
     } else if (currentRoom === 'bossRoom' && currentWorld === 4) {
-      playMusic(theCorrupted)
+      playMusic(boss2)
     } else if (currentRoom === 'bossRoom' && currentWorld === 5) {
+      playMusic(the13thReflection)
+    } else if (currentRoom === 'bossRoom' && currentWorld === 6) {
+      playMusic(theCorrupted)
+    } else if (currentRoom === 'bossRoom' && currentWorld === 7) {
       playMusic(lordOfTheCastle)
     }
   }, [currentRoom])
@@ -87,18 +99,22 @@ function App() {
     if (currentWorld === 1 && playingMusic) {
       playMusic(adventure)
     } else if (currentWorld === 2 && playingMusic) {
-      playMusic(daemons)
+      playMusic(beach)
     } else if (currentWorld === 3 && playingMusic) {
-      playMusic(daemons)
+      playMusic(monstrousMonstro)
     } else if (currentWorld === 4 && playingMusic) {
-      playMusic(danceToTheDeath)
+      playMusic(daemons)
     } else if (currentWorld === 5 && playingMusic) {
+      playMusic(scherzo)
+    } else if (currentWorld === 6 && playingMusic) {
+      playMusic(danceToTheDeath)
+    } else if (currentWorld === 7 && playingMusic) {
       playMusic(edgeOfExistence)
     }
   }, [currentWorld, playingMusic])
 
   useEffect(() => {
-    if(heroStats.experience >= heroStats.experienceToLevelUp) {
+    if (heroStats.experience >= heroStats.experienceToLevelUp) {
       dispatch(gainLevel())
       dispatch(setLevelingUp(true))
       console.log('hero levels up!')
@@ -127,59 +143,59 @@ function App() {
     }
   }, [resettingGame])
 
-  
+
 
   return (
-<div className='board'>
-  
-  <div className='inRoom-display' style={{display: inRoom ? 'none' : undefined}}>
-    {
-      !gameOver
-      ?
-      (
-        <>
-        <div id='current-room' className='map'>
-<RoomButton inRoom={inRoom} />
-          {bossBattle ? <button id='map-healing-room-button' className='room-button' onClick={handleHealingRoom} disabled={inRoom}>Rest</button> : <RoomButton inRoom={inRoom} />}
-          <RoomButton inRoom={inRoom} />
-          <RoomButton inRoom={inRoom} />
-          {bossBattle ? <button className='room-button boss-room-button' onClick={handleBossRoom} disabled={inRoom}>Boss</button> : <RoomButton inRoom={inRoom} />}
-          <RoomButton inRoom={inRoom} />
-          <RoomButton inRoom={inRoom} />
-          {bossBattle ? <button onClick={handleShopRoom} id='map-shop-room-button' className='room-button'>Shop</button> : <RoomButton inRoom={inRoom} />}
-          <RoomButton inRoom={inRoom} />
-          </div>
-          <div className={`overlay ${randomEncounterAnimation && 'flash-white'} `}></div>
-          </>
-      )
-          :
-          <div id='current-room'>
-            <GameOver />
-          </div>
-          
-          
-    }
-  </div>
-     {
-      inRoom &&
-      <div id='current-room'>
-      {roomState === 'enemyRoom' && <Enemy />}
-      {roomState === 'treasureRoom' && <TreasureRoom />}
-      {roomState === 'healingRoom' && <HealingRoom />}
-      {roomState === 'weaponRoom' && <WeaponRoom />}
-      {roomState === 'armorRoom' && <ArmorRoom />}
-      {roomState === 'bossRoom' && <BossRoom />}
-      {roomState === 'spellRoom' && <SpellRoom />}
-      {roomState === 'shopRoom' && <ShopRoom />}
-      
-    </div>
-     }
+    <div className='board'>
+
+      <div className='inRoom-display' style={{ display: inRoom ? 'none' : undefined }}>
+        {
+          !gameOver
+            ?
+            (
+              <>
+                <div id='current-room' className='map'>
+                  <RoomButton inRoom={inRoom} />
+                  {bossBattle ? <button id='map-healing-room-button' className='room-button' onClick={handleHealingRoom} disabled={inRoom}>Rest</button> : <RoomButton inRoom={inRoom} />}
+                  <RoomButton inRoom={inRoom} />
+                  <RoomButton inRoom={inRoom} />
+                  {bossBattle ? <button className='room-button boss-room-button' onClick={handleBossRoom} disabled={inRoom}>Boss</button> : <RoomButton inRoom={inRoom} />}
+                  <RoomButton inRoom={inRoom} />
+                  <RoomButton inRoom={inRoom} />
+                  {bossBattle ? <button onClick={handleShopRoom} id='map-shop-room-button' className='room-button'>Shop</button> : <RoomButton inRoom={inRoom} />}
+                  <RoomButton inRoom={inRoom} />
+                </div>
+                <div className={`overlay ${randomEncounterAnimation && 'flash-white'} `}></div>
+              </>
+            )
+            :
+            <div id='current-room'>
+              <GameOver />
+            </div>
+
+
+        }
+      </div>
+      {
+        inRoom &&
+        <div id='current-room'>
+          {roomState === 'enemyRoom' && <Enemy />}
+          {roomState === 'treasureRoom' && <TreasureRoom />}
+          {roomState === 'healingRoom' && <HealingRoom />}
+          {roomState === 'weaponRoom' && <WeaponRoom />}
+          {roomState === 'armorRoom' && <ArmorRoom />}
+          {roomState === 'bossRoom' && <BossRoom />}
+          {roomState === 'spellRoom' && <SpellRoom />}
+          {roomState === 'shopRoom' && <ShopRoom />}
+
+        </div>
+      }
       <div id='current-hero'>
         <Hero />
       </div>
     </div>
-   
-    
+
+
   )
 }
 
