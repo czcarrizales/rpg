4eb5b1da3d689d2e9e6e852a075f0a927b01e598
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-18 09:35:17 -0700

    seperate file for enemy and boss pools

---
+++ src/enemyPool.ts
@@ -0,0 +1,400 @@
+export const allEnemiesPool = [
+    {
+        name: 'Poison Plant',
+        health: 10,
+        minAttack: 1,
+        maxAttack: 2,
+        experience:2,
+        world: 1,
+        image: '/images/enemies/world1/plant.png'
+    },
+    {
+        name: 'Goblin',
+        health: 20,
+        minAttack: 1,
+        maxAttack: 4,
+        experience: 4,
+        world: 1,
+        image: '/images/enemies/world1/goblin.png'
+    },
+    {
+        name: 'Wolf',
+        health: 20,
+        minAttack: 3,
+        maxAttack: 5,
+        experience: 5,
+        world: 1,
+        image: '/images/enemies/world1/basicWolf.png'
+    },
+    {
+        name: 'Snake',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 1,
+        image: '/images/enemies/world1/snake.png'
+    },
+    {
+        name: 'Bird',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 1,
+        image: '/images/enemies/world1/bird.png'
+    },
+    {
+        name: 'Killer Fish',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 2,
+        image: '/images/enemies/world2/killerFish.png'
+    },
+    {
+        name: 'Pirate',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 2,
+        image: '/images/enemies/world2/pirate.png'
+    },{
+        name: 'Shark',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 2,
+        image: '/images/enemies/world2/shark.png'
+    },
+    {
+        name: 'Sea Creature',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 2,
+        image: '/images/enemies/world2/seaCreature.png'
+    },
+    {
+        name: 'Butterfly',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 3,
+        image: '/images/enemies/world3/butterfly.png'
+    },
+    {
+        name: 'Fat Bird',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 3,
+        image: '/images/enemies/world3/fatBird.png'
+    },
+    {
+        name: 'Puppeteer',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 3,
+        image: '/images/enemies/world3/puppeteer.png'
+    },
+    {
+        name: 'Treasure Monster',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 3,
+        image: '/images/enemies/world3/treasureMonster.png'
+    },
+    {
+        name: 'Unicorn',
+        health: 15,
+        minAttack: 2,
+        maxAttack: 4,
+        experience: 3,
+        world: 3,
+        image: '/images/enemies/world3/unicorn.png'
+    },
+    {
+        name: 'Pumpkin',
+        health: 20,
+        minAttack: 4,
+        maxAttack: 6,
+        experience: 8,
+        world: 4,
+        image: '/images/enemies/world4/pumpkinEnemy.png'
+    },
+    {
+        name: 'Reaper',
+        health: 40,
+        minAttack: 6,
+        maxAttack: 8,
+        experience: 12,
+        world: 4,
+        image: '/images/enemies/world4/reaper.png'
+    },
+    {
+        name: 'Zombie',
+        health: 40,
+        minAttack: 5,
+        maxAttack: 7,
+        experience: 12,
+        world: 4,
+        image: '/images/enemies/world4/zombie.png'
+    },
+    {
+        name: 'Twin Eyes',
+        health: 20,
+        minAttack: 4,
+        maxAttack: 8,
+        experience: 12,
+        world: 4,
+        image: '/images/enemies/world4/eyes.png'
+    },
+    {
+        name: 'Snow Wolf',
+        health: 35,
+        minAttack: 5,
+        maxAttack: 10,
+        experience: 60,
+        world: 5,
+        image: '/images/enemies/world5/iceWolf.png'
+    },
+    {
+        name: 'Frost Orc',
+        health: 50,
+        minAttack: 4,
+        maxAttack: 15,
+        experience: 60,
+        world: 5,
+        image: '/images/enemies/world5/iceOrc.png'
+    },
+    {
+        name: 'Cold Beast',
+        health: 50,
+        minAttack: 4,
+        maxAttack: 15,
+        experience: 60,
+        world: 5,
+        image: '/images/enemies/world5/coldBeast.png'
+    },
+    {
+        name: 'Ice Lizard',
+        health: 50,
+        minAttack: 4,
+        maxAttack: 15,
+        experience: 60,
+        world: 5,
+        image: '/images/enemies/world5/iceLizard.png'
+    },
+    {
+        name: 'Demon',
+        health: 40,
+        minAttack: 5,
+        maxAttack: 10,
+        experience: 80,
+        world: 6,
+        image: '/images/enemies/world6/demon.png'
+    },
+    {
+        name: 'Fire Blob',
+        health: 30,
+        minAttack: 4,
+        maxAttack: 9,
+        experience: 80,
+        world: 6,
+        image: '/images/enemies/world6/fireBlob.png'
+    },
+    {
+        name: 'Lava Bird',
+        health: 25,
+        minAttack: 7,
+        maxAttack: 11,
+        experience: 80,
+        world: 6,
+        image: '/images/enemies/world6/lavaBird.png'
+    },
+    {
+        name: 'Hell Soul',
+        health: 20,
+        minAttack: 6,
+        maxAttack: 8,
+        experience: 80,
+        world: 6,
+        image: '/images/enemies/world6/soul.png'
+    },
+    {
+        name: 'Disciple',
+        health: 60,
+        minAttack: 10,
+        maxAttack: 12,
+        experience: 100,
+        world: 7,
+        image: '/images/enemies/world7/disciple.png'
+    },
+    {
+        name: 'Mirage',
+        health: 60,
+        minAttack: 10,
+        maxAttack: 12,
+        experience: 100,
+        world: 7,
+        image: '/images/enemies/world7/shadow.png'
+    },
+    {
+        name: 'Shadow Sphere',
+        health: 60,
+        minAttack: 10,
+        maxAttack: 12,
+        experience: 100,
+        world: 7,
+        image: '/images/enemies/world7/shadowBall.png'
+    },
+    {
+        name: 'Wrath',
+        health: 60,
+        minAttack: 10,
+        maxAttack: 12,
+        experience: 100,
+        world: 7,
+        image: '/images/enemies/world7/wrath.png'
+    },
+    {
+        name: 'Agony',
+        health: 80,
+        minAttack: 13,
+        maxAttack: 15,
+        experience: 100,
+        world: 7,
+        image: '/images/enemies/world7/agony.png'
+    }
+]
+
+export const allBossesPool = [
+    {
+        name: 'Dragon',
+        health: 60,
+        minAttack: 7,
+        maxAttack: 10,
+        experience: 40,
+        world: 1,
+        image: '/images/bosses/world1/dragon.png'
+    },
+    {
+        name: 'Giant',
+        health: 50,
+        minAttack: 6,
+        maxAttack: 9,
+        experience: 30,
+        world: 1,
+        image: '/images/bosses/world1/giant.png'
+    },
+    {
+        name: 'Pirate Queen',
+        health: 100,
+        minAttack: 10,
+        maxAttack: 14,
+        experience: 60,
+        world: 2,
+        image: '/images/bosses/world2/pirateQueen.png'
+    },
+    {
+        name: 'Squid Lord',
+        health: 100,
+        minAttack: 10,
+        maxAttack: 14,
+        experience: 60,
+        world: 2,
+        image: '/images/bosses/world2/squidLord.png'
+    },
+    {
+        name: 'King Slime',
+        health: 100,
+        minAttack: 10,
+        maxAttack: 14,
+        experience: 60,
+        world: 3,
+        image: '/images/bosses/world3/kingSlime.png'
+    },
+    {
+        name: 'Cuddles',
+        health: 100,
+        minAttack: 10,
+        maxAttack: 14,
+        experience: 60,
+        world: 3,
+        image: '/images/bosses/world3/cuddles.png'
+    },
+    {
+        name: 'The Eye',
+        health: 100,
+        minAttack: 10,
+        maxAttack: 14,
+        experience: 60,
+        world: 4,
+        image: '/images/bosses/creepyEye.png'
+    },
+    {
+        name: 'Abomination',
+        health: 120,
+        minAttack: 11,
+        maxAttack: 13,
+        experience: 200,
+        world: 4,
+        image: '/images/bosses/monster.png'
+    },
+    {
+        name: 'Ice Dancer',
+        health: 150,
+        minAttack: 15,
+        maxAttack: 20,
+        experience: 300,
+        world: 5,
+        image: '/images/bosses/iceDancer.png'
+    },
+    {
+        name: 'Ice Golem',
+        health: 150,
+        minAttack: 18,
+        maxAttack: 21,
+        experience: 300,
+        world: 5,
+        image: '/images/bosses/iceGolem.png'
+    },
+    {
+        name: 'Blaze Dragon',
+        health: 200,
+        minAttack: 18,
+        maxAttack: 21,
+        experience: 400,
+        world: 6,
+        image: null
+    },
+    {
+        name: 'Magma Horror',
+        health: 200,
+        minAttack: 18,
+        maxAttack: 21,
+        experience: 400,
+        world: 6,
+        image: null
+    },
+    {
+        name: 'Oblivion',
+        health: 300,
+        minAttack: 20,
+        maxAttack: 25,
+        experience: 500,
+        world: 7,
+        image: '/images/bosses/finalBoss.png'
+    }
+]
