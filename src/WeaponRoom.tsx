983fd365c36b8bbb0e37e5dec1e7ae272acae781
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-18 09:39:42 -0700

    weaponPool into seperate file

--- src/WeaponRoom.tsx
+++ src/WeaponRoom.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { takeWeapon } from './slices/heroSlice'
 import { goToMapRoom, setInRoom } from './slices/roomSlice'
 import './WeaponRoom.css'
 import { setNewEquipment } from './slices/gameSlice'
 import { playSound } from './utilities'
 import equipmentAppears from '../public/sounds/equipmentAppears.mp3'
+import select from '../public/sounds/select.mp3'
+import { RootState } from './store'
+import { removeFromCurrentWeaponPool } from './slices/weaponsSlice'
 
 interface Weapon {
   type: string;
@@ -17,60 +20,26 @@ interface Weapon {
 const WeaponRoom = () => {
   const [currentWeapon, setCurrentWeapon] = useState<Weapon | null>(null)
   const [weaponAppearing, setWeaponAppearing] = useState(true)
+  const currentWeaponPool = useSelector((state: RootState) => state.weapons.currentWeaponPool)
   const dispatch = useDispatch()
   const handleTakeWeapon = () => {
     dispatch(takeWeapon(currentWeapon))
     dispatch(goToMapRoom())
     dispatch(setInRoom(false))
     dispatch(setNewEquipment(true))
+    dispatch(removeFromCurrentWeaponPool(currentWeapon))
+    playSound(select)
   }
 
   useEffect(() => {
-    const randomWeapons = [
-        {
-            type: 'weapon',
-            name: 'wooden sword',
-            damage: 2,
-            image: '/images/weapons/wooden-sword.png'
-        },
-        {
-            type: 'weapon',
-            name: 'bronze sword',
-            damage: 4,
-            image: '/images/weapons/bronze-sword.png'
-        },
-        {
-            type: 'weapon',
-            name: 'iron sword',
-            damage: 6,
-            image: '/images/weapons/iron-sword.png'
-        },
-        {
-          type: 'weapon',
-            name: 'marble sword',
-            damage: 8,
-            image: '/images/weapons/marble-sword.png'
-        },
-        {
-          type: 'weapon',
-            name: 'obsidian sword',
-            damage: 10,
-            image: '/images/weapons/obsidian-sword.png'
-        },
-        {
-          type: 'weapon',
-            name: 'gold sword',
-            damage: 12,
-            image: '/images/weapons/gold-sword.png'
-        }
-    ]
-    const randomIndex = Math.floor(Math.random() * randomWeapons.length)
+    const randomIndex = Math.floor(Math.random() * currentWeaponPool.length)
     const newWeapon = {
-      type: randomWeapons[randomIndex].type,
-      name: randomWeapons[randomIndex].name,
-      damage: randomWeapons[randomIndex].damage,
-      image: randomWeapons[randomIndex].image
+      type: currentWeaponPool[randomIndex].type,
+      name: currentWeaponPool[randomIndex].name,
+      damage: currentWeaponPool[randomIndex].damage,
+      image: currentWeaponPool[randomIndex].image
     }
+    console.log(currentWeaponPool.length)
     setCurrentWeapon((prevWeapon) => ({
       ...prevWeapon,
       type: newWeapon.type,
