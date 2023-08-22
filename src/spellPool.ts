87fb59598045b2ea44449bbd79bdb1298ef33874
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-22 09:07:30 -0700

    allSpellsPool in seperate ts file

---
+++ src/spellPool.ts
@@ -0,0 +1,58 @@
+export const allSpellsPool = [
+    {
+      type: 'HEAL',
+      name: 'Cure',
+      points: 20,
+      mana: 5,
+      image: '/images/spells/cure.png'
+    },
+    {
+      type: 'DAMAGE',
+      name: 'Fire',
+      points: 25,
+      mana: 5,
+      image: '/images/spells/fire.png'
+    },
+    {
+      type: 'DAMAGE',
+      name: 'Blizzard',
+      points: 10,
+      mana: 2,
+      image: '/images/spells/blizzard.png'
+    },
+    {
+        type: 'DAMAGE',
+        name: 'Thunder',
+        points: 10,
+        mana: 5,
+        image: '/images/spells/thunder.png'
+    },
+    {
+        type: 'DAMAGE',
+        name: 'Quake',
+        points: 20,
+        mana: 15,
+        image: '/images/spells/quake.png'
+    },
+    {
+        type: 'STATUS',
+        name: 'Poison',
+        mana: 5,
+        points: 2,
+        image: '/images/spells/poison.png'
+    },
+    {
+        type: 'HEROBUFF',
+        name: 'Protect',
+        mana: 1,
+        points: 1,
+        image: '/images/spells/protect.png'
+    },
+    {
+        type: 'STATUS',
+        name: 'Stop',
+        mana: 1,
+        points: 2,
+        image: '/images/spells/stop.png'
+    }
+  ]
