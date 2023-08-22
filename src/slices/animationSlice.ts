e1ecd0101f1798250a5032dfb8031d32ca43eef0
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-22 16:07:32 -0700

    animationSlice for animation states

---
+++ src/slices/animationSlice.ts
@@ -0,0 +1,24 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+const animationSlice = createSlice({
+    name: 'animation',
+    initialState: {
+        stopAnimation: false,
+        healAnimation: false,
+        fireAnimation: false
+    },
+    reducers: {
+        setStopAnimation: (state, action) => {
+            state.stopAnimation = action.payload
+        },
+        setHealAnimation: (state, action) => {
+            state.healAnimation = action.payload
+        },
+        setFireAnimation: (state, action) => {
+            state.fireAnimation = action.payload
+        }
+    }
+})
+
+export const {setStopAnimation, setHealAnimation, setFireAnimation} = animationSlice.actions;
+export default animationSlice.reducer
