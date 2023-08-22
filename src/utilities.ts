40177811dd0a1dc96e22aa98d46347a5ac3282f0
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-22 09:05:36 -0700

    individual playback of select/buy/error/heal sound

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,8 +1,13 @@
 import { useDispatch } from "react-redux";
 import { setEnemyIsAttacked } from "./slices/enemySlice";
 import { setHeroIsAttacked } from "./slices/heroSlice";
+import select from '../public/sounds/select.mp3'
+import buy from '../public/sounds/buy.mp3'
+import error from '../public/sounds/error.mp3'
+import heal from '../public/sounds/heal.mp3'
 
 type DispatchFunction = ReturnType<typeof useDispatch>
+interface AudioType extends HTMLAudioElement {}
 
 export const enemyTakeDamageFlash = (dispatch: DispatchFunction) => {
     setTimeout(() => {
@@ -46,8 +51,35 @@ export const heroTakeDamageFlash = (dispatch: DispatchFunction) => {
       }, 80); // Wait for 100 milliseconds before the first flash
 }
 
-export const playSound = (sound: any) => {
-    const audio = new Audio(sound.src)
+export const playSound = (sound: AudioType) => {
+    sound.volume = 0.35
+    sound.preload = "auto"
+    sound.play()
+}
+
+export const playSelectSound = () => {
+    const audio = new Audio(select)
+    audio.volume = 0.35
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playBuySound = () => {
+    const audio = new Audio(buy)
+    audio.volume = 0.35
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playErrorSound = () => {
+    const audio = new Audio(error)
+    audio.volume = 0.35
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playHealSound = () => {
+    const audio = new Audio(heal)
     audio.volume = 0.35
     audio.preload = "auto"
     audio.play()
