23b9035114edc782dc24e860fada3c2f3d953f45
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-23 13:54:17 -0700

    bossroom has world backgrounds

--- src/BossRoom.tsx
+++ src/BossRoom.tsx
@@ -155,7 +155,7 @@ useEffect(() => {
   return (
     !afterBattle
     ?
-      <div className={`boss-room-container boss-overlay ${animation.stopAnimation ? 'stop-overlay' : ''} ${animation.fireAnimation ? 'fire-overlay' : ''}  ${animation.thunderAnimation ? 'thunder-overlay' : ''} ${animation.poisonAnimation ? 'poison-overlay' : ''} ${animation.quakeAnimation ? 'quake-overlay' : ''} ${animation.blizzardAnimation ? 'blizzard-overlay' : ''}`}>
+      <div className={`boss-room-container ${currentWorld === 1 && 'world-1-enemy-background'} ${currentWorld === 2 && 'sea-background'} ${currentWorld === 3 && 'weird-background'} ${currentWorld === 4 && 'scary-background'} ${currentWorld === 5 && 'snowy-background'} ${currentWorld === 6 && 'fire-background'} ${currentWorld === 7 && 'end-background'} boss-overlay ${animation.stopAnimation ? 'stop-overlay' : ''} ${animation.fireAnimation ? 'fire-overlay' : ''}  ${animation.thunderAnimation ? 'thunder-overlay' : ''} ${animation.poisonAnimation ? 'poison-overlay' : ''} ${animation.quakeAnimation ? 'quake-overlay' : ''} ${animation.blizzardAnimation ? 'blizzard-overlay' : ''}`}>
           <h1 className='boss-room-name'>{currentEnemy.name.toUpperCase()} (HP: {currentEnemy.health})</h1>
           <img className={`enemy-image ${enemyIsAttacked ? 'enemy-attacked':''} boss-image`} src={currentEnemy.image} alt="" />
           <div className='boss-room-details'>
