72b91956798173ade845a27d93e49a3eba255c4c
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-23 10:25:56 -0700

    add more weapons and add world and isNew

--- src/weaponPool.ts
+++ src/weaponPool.ts
@@ -3,36 +3,104 @@ export const allWeaponsPool = [
         type: 'weapon',
         name: 'wooden sword',
         damage: 2,
-        image: '/images/weapons/wooden-sword.png'
+        image: '/images/weapons/wooden-sword.png',
+        world: 1,
+        isNew: false
+    },
+    {
+        type: 'weapon',
+        name: 'wooden staff',
+        damage: 1,
+        image: '/images/weapons/wooden-staff.png',
+        world: 1,
+        isNew: false
     },
     {
         type: 'weapon',
         name: 'bronze sword',
         damage: 4,
-        image: '/images/weapons/bronze-sword.png'
+        image: '/images/weapons/bronze-sword.png',
+        world: 2,
+        isNew: false
+    },
+    {
+        type: 'weapon',
+        name: 'bronze staff',
+        damage: 4,
+        image: '/images/weapons/bronze-staff.png',
+        world: 2,
+        isNew: false
     },
     {
         type: 'weapon',
         name: 'iron sword',
         damage: 6,
-        image: '/images/weapons/iron-sword.png'
+        image: '/images/weapons/iron-sword.png',
+        world: 3,
+        isNew: false
+    },
+    {
+        type: 'weapon',
+        name: 'iron staff',
+        damage: 6,
+        image: '/images/weapons/iron-staff.png',
+        world: 3,
+        isNew: false
+    },
+    {
+        type: 'weapon',
+        name: 'jade sword',
+        damage: 4,
+        image: '/images/weapons/jade-sword.png',
+        world: 4,
+        isNew: false
     },
     {
       type: 'weapon',
         name: 'marble sword',
         damage: 8,
-        image: '/images/weapons/marble-sword.png'
+        image: '/images/weapons/marble-sword.png',
+        world: 4,
+        isNew: false
     },
     {
       type: 'weapon',
         name: 'obsidian sword',
         damage: 10,
-        image: '/images/weapons/obsidian-sword.png'
+        image: '/images/weapons/obsidian-sword.png',
+        world: 5,
+        isNew: false
     },
     {
       type: 'weapon',
         name: 'gold sword',
         damage: 12,
-        image: '/images/weapons/gold-sword.png'
-    }
+        image: '/images/weapons/gold-sword.png',
+        world: 5,
+        isNew: false
+    },
+    {
+        type: 'weapon',
+          name: 'royal sword',
+          damage: 12,
+          image: '/images/weapons/royal-sword.png',
+          world: 6,
+          isNew: false
+      },
+      {
+        type: 'weapon',
+          name: 'royal staff',
+          damage: 12,
+          image: '/images/weapons/royal-staff.png',
+          world: 6,
+          isNew: false
+      },
+    {
+        type: 'weapon',
+        name: 'dragon sword',
+        damage: 4,
+        image: '/images/weapons/dragon-sword.png',
+        world: 7,
+        isNew: false
+    },
 ]
