41474bf4223cbdf38f95fa5bc6c39bdce1050922
Chazz Carrizales <czcarrizales@yahoo.com>
2023-08-23 10:26:09 -0700

    more individual playSound

--- src/utilities.ts
+++ src/utilities.ts
@@ -5,6 +5,15 @@ import select from '../public/sounds/select.mp3'
 import buy from '../public/sounds/buy.mp3'
 import error from '../public/sounds/error.mp3'
 import heal from '../public/sounds/heal.mp3'
+import fire from '../public/sounds/fire.mp3'
+import quake from '../public/sounds/quake.mp3'
+import thunder from '../public/sounds/thunder.mp3'
+import blizzard from '../public/sounds/blizzard.mp3'
+import poison from '../public/sounds/poison.mp3'
+import protect from '../public/sounds/protect.mp3'
+import stop from '../public/sounds/stop.mp3'
+import treasureAppears from '../public/sounds/treasureAppears.mp3'
+import attack from '../public/sounds/attack.mp3'
 
 type DispatchFunction = ReturnType<typeof useDispatch>
 interface AudioType extends HTMLAudioElement {}
@@ -52,35 +61,98 @@ export const heroTakeDamageFlash = (dispatch: DispatchFunction) => {
 }
 
 export const playSound = (sound: AudioType) => {
-    sound.volume = 0.35
+    sound.volume = 0.3
     sound.preload = "auto"
     sound.play()
 }
 
+export const playAttackSound = () => {
+    const audio = new Audio(attack)
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
 export const playSelectSound = () => {
     const audio = new Audio(select)
-    audio.volume = 0.35
+    audio.volume = 0.3
     audio.preload = "auto"
     audio.play()
 }
 
 export const playBuySound = () => {
     const audio = new Audio(buy)
-    audio.volume = 0.35
+    audio.volume = 0.3
     audio.preload = "auto"
     audio.play()
 }
 
 export const playErrorSound = () => {
     const audio = new Audio(error)
-    audio.volume = 0.35
+    audio.volume = 0.3
     audio.preload = "auto"
     audio.play()
 }
 
 export const playHealSound = () => {
     const audio = new Audio(heal)
-    audio.volume = 0.35
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playFireSound = () => {
+    const audio = new Audio(fire)
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playQuakeSound = () => {
+    const audio = new Audio(quake)
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playBlizzardSound = () => {
+    const audio = new Audio(blizzard)
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playThunderSound = () => {
+    const audio = new Audio(thunder)
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playProtectSound = () => {
+    const audio = new Audio(protect)
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playPoisonSound = () => {
+    const audio = new Audio(poison)
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playStopSound = () => {
+    const audio = new Audio(stop)
+    audio.volume = 0.3
+    audio.preload = "auto"
+    audio.play()
+}
+
+export const playTreasureAppearsSound = () => {
+    const audio = new Audio(treasureAppears)
+    audio.volume = 0.3
     audio.preload = "auto"
     audio.play()
 }
